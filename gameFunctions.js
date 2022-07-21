ea1b47e8d300b15c2457d4c4de05ad3961a1f11e
Christopher Stillwell <stillwell.c.m@gmail.com>
2022-07-21 21:19:38 +0900

    Edit runTurn and placeShips

--- gameFunctions.js
+++ gameFunctions.js
@@ -1,8 +1,10 @@
-import { gameBoard } from "./createGameboard";
+import { gameBoard } from "./createGameboard.js";
+import { createDomBoard, playerShipDisplay, turnUpdate, updateCpuBoard, updatePlayerBoard } from "./domFunctions.js";
+import { createPlayer } from "./createPlayer.js";
 
 export const runGame = () => {
-    const user = createPlayer();
-    const computer = createPlayer();
+    // const user = createPlayer();
+    // const computer = createPlayer();
 
     const userBoard = gameBoard()
     const computerBoard = gameBoard()
@@ -10,42 +12,69 @@ export const runGame = () => {
     const userBoardShips = userBoard.getShipList();
     const computerBoardShips = userBoard.getShipList();
 
+    createDomBoard();
+
+    const cpuBoardDivs = document.getElementsByClassName('cpu-board-div')
+    for (let i = 0; i < 100; i++) {
+        cpuBoardDivs[i].addEventListener('click', () => {
+            runTurn(i, userBoard, computerBoard)
+        }, {once: true})
+    }
+
     placeShips(userBoard, userBoardShips)
     placeShips(computerBoard, computerBoardShips)
-
-    runTurns(user, computer)
-    printWinner(user, computer)
+    playerShipDisplay(userBoard)
 }
 
 const placeShips = (board, shipArr) => {
     const coordArr1 = board.coordGenerator(5)
-    const coordArr2 = board.coordGenerator(4)
-    const coordArr3 = board.coordGenerator(3)
-    const coordArr4 = board.coordGenerator(3)
-    const coordArr5 = board.coordGenerator(2)
-
+    console.log(`1: ${coordArr1}`)
     board.placeShip(shipArr[0], coordArr1)
+    const coordArr2 = board.coordGenerator(4)
+    console.log(`2: ${coordArr2}`)
     board.placeShip(shipArr[1], coordArr2)
+    const coordArr3 = board.coordGenerator(3)
+    console.log(`3: ${coordArr3}`)
     board.placeShip(shipArr[2], coordArr3)
+    const coordArr4 = board.coordGenerator(3)
+    console.log(`4: ${coordArr4}`)
     board.placeShip(shipArr[3], coordArr4)
+    const coordArr5 = board.coordGenerator(2)
+    console.log(`5: ${coordArr5}`)
     board.placeShip(shipArr[4], coordArr5)
 }
 
-const runTurns = (player1, player2) => {
+export const runTurn = (playerInput, player1, player2) => {
     let gameOver = false
-
-    while (!gameOver) {
-        //prompt player1
-        let playerInput = prompt("Please input attack coordinates", "")
-        player2.recieveAttack(parseInt(playerInput));
-        //update screen
-        if (player2.allSunk()) return gameOver = true
-        if(!gameOver) {
-            player1.recieveAttack(player2.genSingleCoord())
-        }
-        if (player1.allSunk()) return gameOver = true
-        //update screen
+    let playerAttack = parseInt(playerInput)
+    console.log(playerAttack)
+    let isCpuHit = player2.recieveAttack(playerAttack);
+    if (isCpuHit != 'miss') {
+        console.log(`Player: ${playerInput} is a hit`)
+        //In the future, run some domFunction here print hit
     }
+    updateCpuBoard(player2)
+    if (player2.allSunk()) gameOver = true
+    if(!gameOver) {
+        turnUpdate('CPU')
+        setTimeout(cpuAttack(player1, player2), 1000)
+    }
+    if (player1.allSunk()) {
+        gameOver = true
+    } else {
+        turnUpdate('Player 1')
+    }
+    //make a gameover function to handle each of these - update dom and remove event listeners from player-board-div
+    //update screen
+}
+
+const cpuAttack = (player1, player2) => {
+    let cpuAttack = player2.genSingleCoord()
+    let isPlayerHit = player1.recieveAttack(cpuAttack)
+    if (isPlayerHit != 'miss') {
+        console.log(`CPU: ${cpuAttack} is a hit`)
+    }
+    updatePlayerBoard(player1)
 }
 
 const printWinner = (player1, player2) => {
